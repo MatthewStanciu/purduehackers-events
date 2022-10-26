08b1beb11d2f261f983119e3f9ce1f75f5de326d
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-25 20:53:19 -0400

    Remove a tag from `<Link>`s

--- components/event-card.tsx
+++ components/event-card.tsx
@@ -13,37 +13,36 @@ const EventCard = ({
   start: string
   end: string
 }) => (
-  <Link href={`/${slug}`} passHref>
-    <a
-      className={`col-span-1 shadow-lg dark:shadow-black/25 flex flex-col rounded-lg justify-center p-5 ${
-        past(start)
-          ? 'bg-gray-200 dark:bg-gray-700'
-          : 'bg-amber-400 dark:bg-amber-500 hover:scale-105 transform transition'
-      }`}
+  <Link
+    href={`/${slug}`}
+    className={`col-span-1 shadow-lg dark:shadow-black/25 flex flex-col rounded-lg justify-center p-5 ${
+      past(start)
+        ? 'bg-gray-200 dark:bg-gray-700'
+        : 'bg-amber-400 dark:bg-amber-500 hover:scale-105 transform transition'
+    }`}
+    passHref
+  >
+    <p className={past(start) ? '' : 'text-black dark:text-black transition'}>
+      {start === 'TBD'
+        ? 'TBD'
+        : tt(`${past(start) ? '{MM} {Do}, {YYYY} •' : '{dddd}. {MM} {Do} •'}`)
+            .render(new Date(start))
+            .replace('day', '')
+            .replace('nes', '')
+            .replace('Satur', 'Sat')}{' '}
+      {start === 'TBD'
+        ? ''
+        : tt(`{h}:{mm}${end === 'TBD' ? ' {a}' : ''}`).render(new Date(start)) +
+          '—'}
+      {end === 'TBD' ? '???' : tt('{h}:{mm} {a}').render(new Date(end))}
+    </p>
+    <h3
+      className={`${name.length < 30 ? 'text-2xl' : 'text-2xl sm:text-xl'} ${
+        past(start) ? '' : ' text-black dark:text-black transition'
+      } font-bold`}
     >
-      <p className={past(start) ? '' : 'text-black dark:text-black transition'}>
-        {start === 'TBD'
-          ? 'TBD'
-          : tt(`${past(start) ? '{MM} {Do}, {YYYY} •' : '{dddd}. {MM} {Do} •'}`)
-              .render(new Date(start))
-              .replace('day', '')
-              .replace('nes', '')
-              .replace('Satur', 'Sat')}{' '}
-        {start === 'TBD'
-          ? ''
-          : tt(`{h}:{mm}${end === 'TBD' ? ' {a}' : ''}`).render(
-              new Date(start)
-            ) + '—'}
-        {end === 'TBD' ? '???' : tt('{h}:{mm} {a}').render(new Date(end))}
-      </p>
-      <h3
-        className={`${name.length < 30 ? 'text-2xl' : 'text-2xl sm:text-xl'} ${
-          past(start) ? '' : ' text-black dark:text-black transition'
-        } font-bold`}
-      >
-        {name}
-      </h3>
-    </a>
+      {name}
+    </h3>
   </Link>
 )
 
