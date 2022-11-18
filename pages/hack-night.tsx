dab0fa9451f3e9e4d08916232c14bb3122ccaf06
Matthew Stanciu <mattbstanciu@gmail.com>
2022-11-18 16:39:25 -0500

    basic page with nothing on it

--- pages/hack-night.tsx
+++ pages/hack-night.tsx
@@ -1,13 +1,39 @@
+import { orderBy } from 'lodash'
 import { GetStaticProps } from 'next'
 import { fetchEvents } from '../lib/fetchEvents'
+import Nav from '../components/nav'
+import Head from 'next/head'
 
 const HackNight = ({ events }: { events: PHEvent[] }) => {
+  const ogUrl = `https://og.purduehackers.com/Hack%20Night.png?theme=dark&md=1&fontSize=250px&caption=${encodeURIComponent(
+    'Every Friday • 7pm • WALC 1087'
+  )}`
   return (
-    <div className="flex flex-row px-20">
-      {events.map((event) => (
-        <div className="p-4 rounded-lg bg-amber-500">{event.name}</div>
-      ))}
-    </div>
+    <>
+      <Nav />
+      <div className="min-h-screen overflow-hidden flex flex-col font-title dark:bg-gray-900">
+        <Head>
+          <meta property="og:site_name" content="Purdue Hackers" />
+          <meta property="og:name" content="Hack Night — Purdue Hackers" />
+          <meta property="og:title" content="Hack Night — Purdue Hackers" />
+          <meta property="og:image" content={ogUrl} />
+          <meta
+            property="og:description"
+            content="Every Friday night, uninterrupted time to work on personal projects. ✨🧑‍🔬"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:type" content="website" />
+          <title>Hack Night — Purdue Hackers</title>
+        </Head>
+        <div className="flex flex-col grow-0 items-center justify-top w-full flex-1 px-5 pb-8 sm:pb-16 text-center sm:px-20 bg-gray-100 dark:bg-gray-800">
+          <div className="mt-20 sm:mt-28">
+            <h1 className="text-4xl sm:text-7xl lg:text-8-xl font-bold text-amber-450 dark:text-amber-500">
+              Hack Night
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
@@ -15,7 +41,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const events = await fetchEvents('Hack Night')
 
   return {
-    props: { events },
+    props: { events: orderBy(events, 'end', 'desc') },
     revalidate: 10
   }
 }
