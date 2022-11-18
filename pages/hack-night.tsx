18239f88c786f3dfc20e5c45bfe9971d33404bb3
Matthew Stanciu <mattbstanciu@gmail.com>
2022-11-18 16:22:02 -0500

    Update hack-night.tsx

--- pages/hack-night.tsx
+++ pages/hack-night.tsx
@@ -2,11 +2,13 @@ import { GetStaticProps } from 'next'
 import { fetchEvents } from '../lib/fetchEvents'
 
 const HackNight = ({ events }: { events: PHEvent[] }) => {
-  ;<div className="flex flex-row px-20">
-    {events.map((event) => (
-      <div className="p-4 rounded-lg bg-amber-500">{event.name}</div>
-    ))}
-  </div>
+  return (
+    <div className="flex flex-row px-20">
+      {events.map((event) => (
+        <div className="p-4 rounded-lg bg-amber-500">{event.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
